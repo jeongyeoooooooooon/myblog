402e2e68b18541a88688a806be6faa84dd3b9cbe
jeongyeon <joyr1226@likelion.org>
2019-06-03 22:17:39 +0900

    asdsadsa

--- secondproject/settings.py
+++ secondproject/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECRET_KEY = 'ios7vifcotnwo0(n*(5$)it)lyl)t9anvmmn0tufxmwl0!wnwk'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'ios7vifcotnwo0(n*(5$)it)lyl)t9anvmmn0tufxmwl0!wnwk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
